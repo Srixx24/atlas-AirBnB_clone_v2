79fe3e1d5a9584376b55597f7319f05db3102de0
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-10 19:35:58 -0700

    Edits

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,12 +10,8 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    template = '7-states_list.html'
-    states = storage.all(State).values()
-    return render_template(template, states=states)
-    
-    #return render_template("7-states_list.html",
-    #                       states=storage.all("State").values())
+    return render_template("7-states_list.html",
+                           states=storage.all("State").values())
 
 
 @app.teardown_appcontext
