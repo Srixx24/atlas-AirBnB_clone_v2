12c90a6363194b65eb8ad5b28749730bfdff9f6a
Srixx24 <144152489+Srixx24@users.noreply.github.com>
2024-03-10 23:51:48 -0700

    Edits

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -5,10 +5,17 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
+from models.amenity import Amenity
 app = Flask(__name__)
 
 
-
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    template = '10-hbnb_filters.html'
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template(template, states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 app = Flask(__name__)
 
 
